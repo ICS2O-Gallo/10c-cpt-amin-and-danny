b1454aef446ef286aa7798150d1e8951ca059153
AminHoss <47458405+AminHoss@users.noreply.github.com>
2019-06-10 12:44:29 -0400

    Update DemoBuilding.py

--- Development/Incomplete/DemoBuilding.py
+++ Development/Incomplete/DemoBuilding.py
@@ -1,7 +1,31 @@
 import os
 import arcade
 
+
+WIDTH = 1300
+HEIGHT = 700
+
+
 file_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(file_path)
+
 tile_img = arcade.load_texture('/home/robuntu/Hosseini/untitled/download.jpeg')
-arcade.draw_xywh_rectangle_textured(x, y, tile_img.width, tile_img.height, tile_img)
+
+
+def setup():
+    arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(update, 1 / 60)
+
+    # Override arcade window methods
+    window = arcade.get_window()
+    window.on_draw = on_draw
+
+    arcade.run()
+    
+def update(delta_time):
+    pass
+
+def on_draw():
+    arcade.start_render()
+    arcade.draw_xywh_rectangle_textured(100, 200, tile_img.width, tile_img.height, tile_img)

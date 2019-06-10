494d1d9617a4dc108b974b4161cb56dbf9f018e3
AminHoss <47458405+AminHoss@users.noreply.github.com>
2019-06-10 12:41:34 -0400

    Update DemoBuilding.py

--- Development/Incomplete/DemoBuilding.py
+++ Development/Incomplete/DemoBuilding.py
@@ -1,1 +1,7 @@
+import os
+import arcade
 
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
+tile_img = arcade.load_texture('/home/robuntu/Hosseini/untitled/download.jpeg')
+arcade.draw_xywh_rectangle_textured(x, y, tile_img.width, tile_img.height, tile_img)
